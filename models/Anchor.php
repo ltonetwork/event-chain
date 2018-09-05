16a3431a1eae928ff0454db5c6e6795c7d3c57f6
Moesjarraf J <moesjarraf@gmail.com>
2018-09-05 15:36:00 +0200

    Add anchor service

---
+++ models/Anchor.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Class to interact with anchor service
+ */
+class Anchor
+{
+    /**
+     * @var object
+     */
+    protected $config;
+
+    /**
+     * @var GuzzleHttp\ClientInterface
+     */
+    protected $httpClient;
+    
+    
+    /**
+     * Class constructor
+     * 
+     * @param object|array      $config
+     * @param GuzzleHttp\Client $httpClient
+     */
+    public function __construct($config, GuzzleHttp\ClientInterface $httpClient)
+    {
+        $this->config = (object)$config;
+        $this->httpClient = $httpClient;
+    }
+    
+    
+    /**
+     * Anchor the given hash
+     * 
+     * @param string  $hash
+     * @param string  $encoding
+     */
+    public function hash($hash, $encoding)
+    {
+        $endpoint = $this->config->url;
+        $url = "{$endpoint}/hash";
+        $payload = compact('hash', 'encoding');
+        
+        $options = [
+            'json' => $payload,
+            'http_errors' => true,
+            'query' => []
+        ];
+        
+        $this->httpClient->post($url, $options);
+    }
+}
