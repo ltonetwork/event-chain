d5907894e8159cc146eff76564006c9b5ca0ca37
Arnold Daniels <arnold@jasny.net>
2019-03-17 04:15:03 +0100

    Composer update Switch args for Jasny HttpDigest

--- declarations/generic/http-digest.php
+++ declarations/generic/http-digest.php
@@ -3,13 +3,12 @@
 use Psr\Container\ContainerInterface;
 use Jasny\HttpDigest\HttpDigest;
 use Jasny\HttpDigest\ClientMiddleware;
-use Jasny\HttpDigest\Negotiation\DigestNegotiator;
 
 return [
-    HttpDigest::class => function (ContainerInterface $container) {
-        return new HttpDigest(new DigestNegotiator(), 'SHA-256');
+    HttpDigest::class => static function () {
+        return new HttpDigest('SHA-256');
     },
-    ClientMiddleware::class => function(ContainerInterface $container) {
+    ClientMiddleware::class => static function(ContainerInterface $container) {
         $service = $container->get(HttpDigest::class);
 
         return new ClientMiddleware($service);
