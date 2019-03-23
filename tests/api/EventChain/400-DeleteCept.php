a92d96912a11368eddae3e574e58cfdd6746bcf9
Sergey Boltonosov <minstel@yandex.ru>
2019-03-23 22:17:38 +0200

    Minor tests fixes

--- tests/api/EventChain/400-DeleteCept.php
+++ tests/api/EventChain/400-DeleteCept.php
@@ -5,7 +5,7 @@
  */
 
 $I = new ApiTester($scenario);
-$I->wantTo('Get existing event chain');
+$I->wantTo('delete existing event chain');
 
 $I->amSignatureAuthenticated("LtI60OqaM/gZbaeN8tWBJqOy7yiPwxSMZDo/aQvsPFzbJiGUQZ2iyDtBkL/+GJseJnUweTabuOn8RtR4V3MOKw=="); // wJ4WH8dD88fSkNdFQRjaAhjFUZzZhV5yiDLDwNUnp6bYwRXrvWV8MJhQ9HL9uqMDG1n7XpTGZx7PafqaayQV8Rp
 
